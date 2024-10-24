a157e90a38ebdca128f2ec0fcda2d30d874679c7
KyunyeonKim <kgn4746@naver.com>
2024-10-24 17:46:02 +0900

    feat:validation check기능 및 css 수정중

--- src/main/java/com/tmtb/pageon/login/controller/LoginController.java
+++ src/main/java/com/tmtb/pageon/login/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Slf4j
 @Controller
 public class LoginController {
@@ -20,26 +22,25 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginVO loginVO, Model model, HttpSession httpSession) {
+    public String login(@ModelAttribute LoginVO loginVO, HttpSession httpSession, RedirectAttributes redirectAttributes) {
         // ID와 PW가 비어있는지 확인
         if (loginVO.getId() == null || loginVO.getId().isEmpty() || loginVO.getPw() == null || loginVO.getPw().isEmpty()) {
-            model.addAttribute("error", "아이디와 비밀번호를 모두 입력해야 합니다.");
-            return "login"; // 로그인 페이지의 뷰 이름을 반환
+            redirectAttributes.addFlashAttribute("error", "아이디와 비밀번호를 모두 입력해야 합니다.");
+            return "redirect:/"; // 로그인 페이지로 리다이렉트
         }
 
+        // 로그인 처리
         String login = loginService.login(loginVO.getId(), loginVO.getPw());
 
         if (login != null) {
             httpSession.setAttribute("id", login);
-            log.info("로그인 기능 성공");
             log.info("로그인 성공, 세션에 id 저장: " + login);
             return "redirect:/"; // 메인 페이지로 리다이렉트
         } else {
-            model.addAttribute("error", "로그인 아이디가 없습니다. 다시 시도해주세요.");
-            return "login"; // 로그인 페이지의 뷰 이름을 반환
+            redirectAttributes.addFlashAttribute("error", "로그인 아이디가 없습니다. 다시 시도해주세요.");
+            return "redirect:/"; // 로그인 실패 시 다시 로그인 페이지로 리다이렉트
         }
     }
-
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate(); // 세션 무효화
