87802bff304abd05b2fdd6581f8b5c8377105cbd
홍석표 <whdk333@nate.com>
2024-10-25 18:02:28 +0900

    community메인 페이지에서 각 글마다 링크 기능 추가

--- src/main/java/com/tmtb/pageon/community/controller/CommunityController.java
+++ src/main/java/com/tmtb/pageon/community/controller/CommunityController.java
@@ -1,6 +1,7 @@
 package com.tmtb.pageon.community.controller;
 
 import com.tmtb.pageon.board.model.BoardVO;
+import com.tmtb.pageon.notice.model.NoticeVO;
 import com.tmtb.pageon.review.model.ReviewVO;
 import com.tmtb.pageon.community.model.CommunityVO;
 import com.tmtb.pageon.community.service.CommunityService;
@@ -26,7 +27,7 @@ public class CommunityController {
         log.info("/n_community");
 
         List<BoardVO> list1 = service.boardCommunity();
-        List<CommunityVO> list2 = service.noticeCommunity();
+        List<NoticeVO> list2 = service.noticeCommunity();
         List<ReviewVO> list3 = service.reviewCommunity();
         List<ForumVO> list4 = service.forumCommunity();
 
@@ -39,6 +40,46 @@ public class CommunityController {
         model.addAttribute("list2", list2);
         model.addAttribute("list3", list3);
         model.addAttribute("list4", list4);
+
+        return "community/community_main";
+    }
+
+    @GetMapping("/board/b_selectOne.do")
+    public String selectOne(BoardVO vo, Model model) {
+        log.info("/board/b_selectOne.do");
+        log.info("vo:{}", vo);
+
+        BoardVO vo2 = service.boardSelectOne(vo);
+        log.info("vo2:{}", vo2);
+
+        model.addAttribute("vo2", vo2);
+
+        return "community/community_main";
+    }
+
+    @GetMapping("/review/r_selectOne.do")
+    public String selectOne(ReviewVO vo, Model model) {
+        log.info("/review/r_selectOne.do");
+        log.info("vo:{}", vo);
+
+        ReviewVO vo2 = service.reviewSelectOne(vo);
+        log.info("vo2:{}", vo2);
+
+        model.addAttribute("vo2", vo2);
+
+        return "community/community_main";
+    }
+
+
+    @GetMapping("/forum/f_selectOne.do")
+    public String selectOne(ForumVO vo, Model model) {
+        log.info("/forum/f_selectOne.do");
+        log.info("vo:{}", vo);
+
+        ForumVO vo2 = service.forumSelectOne(vo);
+        log.info("vo2:{}", vo2);
+
+        model.addAttribute("vo2", vo2);
 
         return "community/community_main";
     }
--- src/main/java/com/tmtb/pageon/community/mapper/CommunityMapper.java
+++ src/main/java/com/tmtb/pageon/community/mapper/CommunityMapper.java
@@ -1,6 +1,7 @@
 package com.tmtb.pageon.community.mapper;
 
 import com.tmtb.pageon.board.model.BoardVO;
+import com.tmtb.pageon.notice.model.NoticeVO;
 import com.tmtb.pageon.review.model.ReviewVO;
 import com.tmtb.pageon.community.model.CommunityVO;
 import com.tmtb.pageon.forum.model.ForumVO;
@@ -13,13 +14,19 @@ import java.util.List;
 public interface CommunityMapper {
 
 
-    public List<CommunityVO> noticeCommunity();
+    public List<NoticeVO> noticeCommunity();
 
     public List<ForumVO> forumCommunity();
 
     public List<ReviewVO> reviewCommunity();
 
     public List<BoardVO> boardCommunity();
+
+    public ReviewVO reviewSelectOne(ReviewVO vo);
+
+    public ForumVO forumSelectOne(ForumVO vo);
+
+    public BoardVO boardSelectOne(BoardVO vo);
 }
 
 
