76ab178a10a2983e892b01307fbff2da4d92a27a
Lee Sumin <bluky0516@gmail.com>
2024-11-04 19:01:42 +0900

    비 로그인 시 작품 메인에서 선호 카테고리 목록이 안나오도록 수정

--- src/main/java/com/tmtb/pageon/HomeController.java
+++ src/main/java/com/tmtb/pageon/HomeController.java
@@ -15,6 +15,7 @@ import com.tmtb.pageon.webnovel.model.WebnovelVO;
 import com.tmtb.pageon.webnovel.service.WebnovelService;
 import com.tmtb.pageon.webtoon.model.WebtoonVO;
 import com.tmtb.pageon.webtoon.service.WebtoonService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -86,22 +87,26 @@ public class HomeController {
 
 
     @GetMapping("/work_index")
-    public String workPage(Model model) {
+    public String workPage(Model model, HttpSession session) {
         log.info("작품 메인 페이지");
 
-        // 테스트용 더미 사용자 ID
-        String id = "tester2";
-        UserVO user = userService.findById(id);
-        List<String> likeCategories = Arrays.asList(user.getLike_categories().split(","));
-        model.addAttribute("likeCategories", likeCategories);
+        // 세션에서 사용자 ID 가져오기
+        String id = (String) session.getAttribute("id");
+        log.info("세션에서 가져온 사용자 ID: {}", id);
 
-        // 선호 카테고리 기반 웹소설 목록 조회
-        List<WebnovelVO> likedWebnovels = webnovelService.selectWebnovelsByCategories(likeCategories, 1, 20, "latest");
-        model.addAttribute("likedWebnovels", likedWebnovels);
+        if (id != null) {
+            UserVO user = userService.findById(id);
+            List<String> likeCategories = Arrays.asList(user.getLike_categories().split(","));
+            model.addAttribute("likeCategories", likeCategories);
 
-        // 선호 카테고리 기반 도서 목록 조회
-        List<BookVO> likedBooks = bookService.selectBooksByCategories(likeCategories, 1, 20, "latest");
-        model.addAttribute("likedBooks", likedBooks);
+            // 선호 카테고리 기반 웹소설 목록 조회
+            List<WebnovelVO> likedWebnovels = webnovelService.selectWebnovelsByCategories(likeCategories, 1, 20, "latest");
+            model.addAttribute("likedWebnovels", likedWebnovels);
+
+            // 선호 카테고리 기반 도서 목록 조회
+            List<BookVO> likedBooks = bookService.selectBooksByCategories(likeCategories, 1, 20, "latest");
+            model.addAttribute("likedBooks", likedBooks);
+        }
 
         // 인기순으로 20개의 웹툰 조회
         List<WebtoonVO> popularWebtoons = webtoonService.selectPopularWebtoons(1, 20);
